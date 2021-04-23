af6e62d1d7f168798402e2838ab564c922b43818
Julian075 <70178887+Julian075@users.noreply.github.com>
2021-04-23 16:57:13 -0500

    lo que hicimos de 2:30 - 5pm

--- addsub754_JJ.sv
+++ addsub754_JJ.sv
@@ -15,10 +15,8 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 	reg [24:0] result=24b'0;
 	reg mayA=0;
 	
-	signal aux [23:0];// para correr mantiza
-	signal diferencia [7:0]// variable para ver la diferencia entre exp
-	signal uno=0; // variable auxiliar para normalizar el resultado
-	signal i=0;
+	reg aux [23:0];// para correr mantiza
+	reg diferencia [7:0]// variable para ver la diferencia entre exp
 	
 	State currentState, nextState;
 	
@@ -50,7 +48,7 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 	S1: begin //1  //etapa para mirar casos especiales y de no ser el caso mirar diferencia entre exp
 		if (start == 1 and ready ==0) begin //2
 		
-			if((expA !=5'b00000 and expB !=5'b00000) and (expA !=5'b11111 and expB !=5'b11111) ) begin //3
+			if((expA !=5'b00000 and expB !=5'b00000) and (expA !=5'b11111 and expB !=5'b11111) and ((A != B) or ( (A==B)  and (oper==0)))  ) begin //3
 			
 				//se verifica que los exp no entren dentro de las excepciones 
 				if (expA > expB) begin //4
@@ -103,7 +101,7 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 			end //6	
 		
 		
-			else  begin //10 // se mira si los exp de algunos de las datos ingresados estan reservados para el caso del NAn o infinito
+			else if(expA !=5'b11111 or expB !=5'b11111)  begin //10 // se mira si los exp de algunos de las datos ingresados estan reservados para el caso del NAn o infinito
 		
 			if(expA== 5'b11111) begin 
 			   R<=A; // en caso que alguno sea Nan o infinito el resultado sera ese numero
@@ -120,6 +118,18 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 			end//10	
 			
 			
+			else begin // A=B
+			
+			
+				R<= 2'h00; // en caso que A=B  y sea resta A-B=0
+				ready <= 1;
+				nextState <= S11; 
+	
+				
+			
+			end
+		
+		
 		end//2
 		
 		else
@@ -158,21 +168,22 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 			
 				R[31] <= A[31]; //signo de la salida va a ser el signo de cualquiera de los dos  numeros ya que tienen igual signo
 			
-				if (mayA == 1 and diferencia != 0)
+				if (mayA == 1 and diferencia != 0) //A>B
 					result <= mantA + aux;
 					nextState <= S4;
 					
 					
-				else if (mayA == 0 and diferencia != 0 )
+				else if (mayA == 0 and diferencia != 0 ) //B>A
 					result <= mantB + aux;
 					nextState <= S4;
 					
-				else if(diferencia == 0)
+				else if(diferencia == 0) // exp iguales
 					result <= mantB + mantA;
 					nextState <= S4;
+					//melo
 			end
 			
-			else begin //A y B signos diferentes
+			else begin //A y B signos diferentes, suma
 				
 				if (mayA == 1 and diferencia != 0) begin
 					result <= mantA + aux;
@@ -190,7 +201,7 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 					
 				end
 										 
-				else if(diferencia == 0) begin
+				else if(diferencia == 0) begin //exp iguales
 					result <= mantB + mantA;
 					if (mantA > mantB) begin
 							R[31] <= A[31]; // si se suman el resultado tendra el signo del numero mayor, en este caso A
@@ -198,14 +209,19 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 							nextState <= S4;
 																 
 					end											 
-				   else begin
+				   else if (mantB > mantA) begin
 						
 						R[31] <= B[31]; // de caso contrario mantisa de B mas grando, por tanto result tiene el signo de B
 						nextState <= S4;
 					
 					end
+					else begin
+						
+						R<= 2'h00;
 					
-				end
+					end
+					
+				end //suma se supone que esta bien
 				
 			end
 				
@@ -217,29 +233,57 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 		if(A[31]==B[31]) begin //ambos numeros con signos iguales
 		
 			if (mayA == 1 and diferencia != 0) begin//A>B
-					result <= mantA - aux; //signo del resultado es el de A
-					R[31] <= A[31];
-					nextState <= S4;
+					result <= mantA - aux; //signo del resultado es el de A //arreglar
+					
+					if(A[31]==0) begin // los dos positivos A-B , siempre signo de A
+						R[31] <= A[31];
+						nextState <= S4;
+					end
+					
+					if(A[31]==1) begin  // los dos son negativos A-B da el signo contrario a A
+						R[31] <= ~A[31];
+						nextState <= S4;
+					end
 			end
 					
 					
 			else if (mayA == 0 and diferencia != 0 ) begin//B>A
+			
+			if(A[31]==0) begin  // los dos son positivos
 					result <= mantB - aux; 
-					R[31] <= ~B[31];      //signo del resultado es el contrario al de B
+					R[31] <= ~B[31];      //signo del resultado es el contrario al de B //
 					nextState <= S4;
 			end
+			
+			if(A[31]==1) begin  // los dos son negativos
+					result <= mantB - aux; 
+					R[31] <= B[31];      //signo del resultado es el de B //
+					nextState <= S4;			
 					
-			else if(diferencia == 0) begin
-					if(mayA) begin 
-						result <= mantA - mantB;
+			end
+			
+			
+			
+			
+			
+			
+			end
+					
+			else if(diferencia == 0) begin // exponentes iguales
+				if(A[31]==0) begin //positivos 
+				if(mantA > mantB) begin 
+						result <= mantA - mantB; //A-B .A>B. 10-5=5
 						R[31] <= A[31]; //signo del resultado es el de A
 						nextState <= S4;
 					end
-					if(mayB) begin
-						result <= mantB - mantA;
-						R[31] <= ~B[31]; //signo del resultado es el contrario al de B
-						nextState <= S4;
+				else if(mantB > mantA) begin
+					result <= mantB - mantA; //A-B .B>A. 5-10=-5
+					R[31] <= ~A[31]; //signo del resultado es el contrario al de A
+					nextState <= S4;
 					end
+				end //melo
+				
+					
 				end
 			end
 		
@@ -272,7 +316,7 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 		end
 		
 		
-		end
+		end//melo
 		
 	end
 	
@@ -282,7 +326,90 @@ module addsub754_JJ(clk, reset, start, oper, A, B, R, ready);
 	
 	end
 	
-	S4: begin // normalizar la mantisa  //10.00 //11.11 //1.0 //0.1
+	S4: begin // normalizar la mantisa  //10.00 //11.11 //01.0 //0.1
+	if(start == 1 and ready ==0)begin
+			if(oper==0) begin //suma  
+			
+				if(result[24]==1) begin
+					R[22:0] <= result[23:1];//11.11111
+					//restarle uno al exponente
+					
+					if(mayA) begin
+					
+						R[30:23] <= expA -1;
+						nextState <= S5;
+						
+					end
+					
+					else begin
+					
+						R[30:23] <= expB -1;
+						nextState <= S5;
+						
+					end
+					
+				end
+				
+				else begin
+				
+					R[22:0] <= result[22:0];//01.11111
+					
+					if(mayA) begin
+					
+						R[30:23] <= expA ;
+						nextState <= S5;
+						
+					end
+					
+					else begin
+					
+						R[30:23] <= expB ;
+						nextState <= S5;
+						
+					end
+				
+				end
+			end
+			
+			else  begin//resta
+			//1.xxx - 0.xxx =01.xxx 	
+			//	1.xxx - 1.xxx =0.xxx
+				if(diferencia != 0) begin
+				
+					R[22:0] <= result[22:0];//01.xxxxx
+					
+					if(mayA) begin
+					
+						R[30:23] <= expA ;
+						nextState <= S5;
+						
+					end
+					
+					else begin
+					
+						R[30:23] <= expB ;
+						nextState <= S5;
+						
+					end
+				end
+				
+				else begin //00.xxxx caso triste
+				
+					
+					
+				end
+					
+				
+			end
+			
+			
+			
+			
+			
+			
+	end
+	
+	else nextState <= S1;
 	
 	
 //		OUT=0;
